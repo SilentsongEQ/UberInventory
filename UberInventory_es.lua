fc1bb5edb111b61adf89afe4bd5cc3914596b3e6
SilentsongEQ <secrettoo410@gmail.com>
2026-04-11 15:30:25 -0400

    first commit

---
+++ UberInventory_es.lua
@@ -0,0 +1,10 @@
+--[[
+    =================================================================
+    Description:
+        All strings (Spanish) used by UberInventory.
+    ================================================================= --]]
+
+-- Strings used within UberInventory
+if (GetLocale()=="esES") then
+
+end;
